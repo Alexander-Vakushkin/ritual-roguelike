bd5483f42383808e06d66e2cdfd062a8581ed6ee
Alexander-Vakushkin <alexander.vakushkin@gmail.com>
2018-04-23 13:34:59 +0300

    Indentation fix

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,7 @@ int main()
     TCOD_renderer_t renderer=TCOD_RENDERER_SDL;
     TCOD_key_t key = {TCODK_NONE,0};
 
-	TCODConsole::initRoot(80,50,"Ritual roguelike game",false,renderer);
+    TCODConsole::initRoot(80,50,"Ritual roguelike game",false,renderer);
     TCODConsole::root->setDefaultForeground(TCODColor::grey);
     TCODConsole::root->setDefaultBackground(TCODColor::black);
     TCODConsole::root->printEx(30,25,TCOD_BKGND_SET,TCOD_LEFT,"Hello from libtcod!");
