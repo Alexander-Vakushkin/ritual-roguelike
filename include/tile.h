72d184d7eaa9a030b8e189332f14b81edf7e62a2
Alexander Vakushkin <alexander.vakushkin@gmail.com>
2018-04-28 00:34:37 +0300

    added tile types

--- include/tile.h
+++ include/tile.h
@@ -12,9 +12,13 @@ public:
     char get_view() const { return m_view; }
     bool is_walkable() const { return m_walkable; }
 
+    static const Tile FLOOR;//{ '.', true };
+    static const Tile WALL;// { '#', false };
+
 private:
     char m_view;
     bool m_walkable;
 };
+
 
 #endif // TILE_H+vD8S/mkyimBAwdTHOeUUQe9C6ngub0+
---
+++ source/tile.cpp
@@ -0,0 +1,4 @@
+#include <tile.h>
+
+const Tile Tile::FLOOR = { '.', true };
+const Tile Tile::WALL = { '#', false };
